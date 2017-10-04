868bed6ca44a2cb4b94131453ef4e05b66467643
wangqing93 <wangqing9305@gmail.com>
2017-10-04 17:11:10 +0800

    29-4 no ac

---
+++ hihocoder/DiffSubNum.cpp
@@ -0,0 +1,116 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        s = extendString(s);
+        vector<int> rad = calRad(s);
+        int maxLen = -1, midIndex = -1;
+        for (int i = 0; i < s.length(); i++)
+        {
+            //cout << rad[i] << endl;
+            if (s[i-rad[i]] == '#')
+                rad[i]--;
+            if (rad[i] > maxLen)
+            {
+                maxLen = rad[i];
+                midIndex = i;
+            }
+        }
+        string res="";
+        for (int i = midIndex - maxLen; i <= midIndex + maxLen; i++)
+        {
+            if (s[i] != '#')
+                res += s[i];
+        }
+        return res;
+
+    }
+    vector<int> calRad(string s)
+    {
+        vector<int> rad;
+        for (int i = 0; i < s.length(); i++)
+            rad.push_back(0);
+        for (int i = 0, j = 1, k; i < s.length(); i+=k)
+        {
+            while((i-j)>=0 && (i+j)<s.length() && s[i-j]==s[i+j])
+                j++;
+            rad[i] = j-1; //i
+            for (k = 1; k <= rad[i] && rad[i]-k != rad[i-k]; k++)
+                rad[i+k] = min(rad[i]-k, rad[i-k]);
+            j = max(j-k, 0);
+        }
+        return rad;
+    }
+    string extendString(string s)
+    {
+        string res;
+        for (int i = 0; i < s.length()-1; i++)
+        {
+            res += s[i];
+            res += '#';
+        }
+        res += s[s.length()-1];
+        return res;
+    }
+    long long countSubstrings(string s)
+    {
+        long long res = 0;
+        s = extendString(s);
+        vector<int> rad = calRad(s);
+        //cout << s << endl;
+        for (int i = 0; i < s.length(); i++)
+        {
+            //cout << i << " " << rad[i] << endl;
+            res += rad[i]/2;
+            if (s[i] != '#')
+                res++;
+            if (s[i-rad[i]] != '#' && rad[i]%2==1)
+                res++;
+        }
+        return res;
+    }
+
+    long long DifferentPD(string s)
+    {
+        long long res = 0;
+        s = extendString(s);
+        vector<int> rad = calRad(s);
+        unordered_map<string, int> mark;
+        //cout << s << endl;
+        for (int i = 0; i < s.length(); i++)
+        {
+            //cout << i << " " << rad[i] << endl;
+            int j = rad[i];
+            for (j; j >= 0; j--)
+            {
+                if (s[i-j] == '#')
+                    continue;
+                string sub = s.substr(i-j, 2*j+1);
+                if (mark.find(sub) == mark.end())
+                {
+                    mark.insert(make_pair(sub, 0));
+                    //cout << sub << endl;
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+};
+
+int main()
+{
+    string s;
+    cin >> s;
+    Solution test;
+    cout << test.DifferentPD(s) << endl;
+    //cout << "Hello world!" << endl;
+    return 0;
+}
