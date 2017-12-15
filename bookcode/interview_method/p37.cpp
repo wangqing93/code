b5ccd60d93711617e8011fe24163bb42a5fbc446
wangqing93 <wangqing9305@gmail.com>
2017-12-15 13:50:30 +0800

    Update p37.cpp

--- bookcode/interview_method/p37.cpp
+++ bookcode/interview_method/p37.cpp
@@ -8,6 +8,9 @@ int main()
     char strs[100] = "daoaudafsfsf";
     cout << strlen(strs) <<endl;  //12
     cout << sizeof strs << endl;  //100
+    char a[] = "aBcDe";
+    cout << strlen(a) <<endl;  //5
+    cout << sizeof a << endl;  //6
     cout << "Hello world!" << endl;
     return 0;
 }
