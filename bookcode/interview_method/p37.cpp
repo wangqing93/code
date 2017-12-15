0a039a7213aac2fea6c3d23806cb7969697de228
wangqing93 <wangqing9305@gmail.com>
2017-12-15 13:42:54 +0800

    Create p37.cpp

---
+++ bookcode/interview_method/p37.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+
+int main()
+{
+    char strs[100] = "daoaudafsfsf";
+    cout << strlen(strs) <<endl;  //12
+    cout << sizeof strs << endl;  //100
+    cout << "Hello world!" << endl;
+    return 0;
+}
