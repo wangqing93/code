7e872c9624dfa59f95cdd278c63dc6aebe90ac0b
wangqing93 <wangqing9305@gmail.com>
2017-12-23 22:38:18 +0800

    Update vector_info.cpp

--- exam_code/sinopec/vector_info.cpp
+++ exam_code/sinopec/vector_info.cpp
@@ -15,3 +15,25 @@ int main()
         cout << t[i];    //111111111
     return 0;
 }
+
+
+/*
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    vector<int> t;
+    t.resize(5,2);
+    t.resize(3,1);
+    for (int i = 0; i < t.size(); i++)
+        cout << t[i];
+    t.resize(6,3);t.reserve(4);
+    for (int i = 0; i < t.size(); i++)
+        cout << t[i];    //222222333
+    return 0;
+}
+
+/*
