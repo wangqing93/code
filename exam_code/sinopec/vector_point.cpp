31f3d199258f5c3cfcd3319fad7409dac0b32c3c
wangqing93 <wangqing9305@gmail.com>
2017-12-23 23:14:22 +0800

    Create vector_point.cpp

---
+++ exam_code/sinopec/vector_point.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a = 1; int b = 2; int *a1 = &a;
+    cout <<a1[0] << endl;
+    *a1 = b;   //1 2 2
+    cout <<a1[0] << endl;
+    b = 4;
+    cout <<a1[0] << endl;
+    cout << "Hello world!" << endl;
+    return 0;
+}
